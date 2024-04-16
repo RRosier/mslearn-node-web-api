afebdbea0573144707417e40c0a0cd64d69eae3b
Ronald Rosier <ronaldrosier@live.be>
2024-04-16 10:36:30 +0000

    middleware

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -2,7 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+function isAuthorized(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || authHeader !== 'secretpassword') {
+        return res.status(401).send('Unauthorized: Access Denied');
+    }
+
+    next();
+}
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/users', isAuthorized, (req, res) => {
+    res.json([
+        {
+            id: 1,
+            name: 'User Userson'
+        }
+    ]);
+});
 
 app.get('/products', (req, res) => {
     const products = [
@@ -12,6 +33,12 @@ app.get('/products', (req, res) => {
     ];
 
     res.json(products);
+});
+
+app.post('/products', (req, res) => {
+    let product = req.body;
+
+    res.json(product);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}! http://localhost:${port}/`));
