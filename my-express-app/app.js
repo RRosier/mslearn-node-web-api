3f8c6e3192f13f653242887b6ddc1811d795c334
Ronald Rosier <ronaldrosier@live.be>
2024-04-16 09:59:32 +0000

    add simple get json api

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -4,4 +4,14 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/products', (req, res) => {
+    const products = [
+        { id: 1, name: 'hamer' },
+        { id: 2, name: 'screwdriver' },
+        { id: 3, name: 'wrench' }
+    ];
+
+    res.json(products);
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}! http://localhost:${port}/`));
